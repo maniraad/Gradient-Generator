00bb499e376bc9f7d871404f50aff02eadca669c
maniraad <maniraad7@gmail.com>
2023-11-05 20:19:21 +0330

    add random btn

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const firstColorInput = $.querySelector('#color-a');
 const secondColorInput = $.querySelector('#color-b');
 const directions = $.querySelectorAll('.buttons button');
 const generatorBtn = $.querySelector('#submit');
+const randomGeneratorBtn = $.querySelector('#random');
 const codeTextArea = $.querySelector('#code');
 const copyBtn = $.querySelector('#copy');
 let currentDirection = 'to top'
@@ -34,7 +35,18 @@ const copyCodeCss = ()=>{
     codeTextArea.select()
     $.execCommand('copy')
 }
+const generateCssCodeRandom = ()=>{
+    let firstNum = Math.floor(Math.random() * 255)
+    let secondNum = Math.floor(Math.random() * 255)
+    let thirdNum = Math.floor(Math.random() * 255)
+    let fourthNum = Math.floor(Math.random() * 255)
+    let fifthNum = Math.floor(Math.random() * 255)
+    let sixthNum = Math.floor(Math.random() * 255)
 
+    let cssCode = `linear-gradient(${currentDirection},rgba(${firstNum},${secondNum},${thirdNum},1),rgba(${fourthNum},${fifthNum},${sixthNum},1))`
+    codeTextArea.value = 'background: ' + cssCode
+    $.body.style.background = cssCode
+}
 directions.forEach(direction => {
     direction.addEventListener('click', (event) => {
         let direction = event.target.dataset.direction
@@ -45,4 +57,8 @@ directions.forEach(direction => {
 
 generatorBtn.addEventListener('click',generateCssCode)
 copyBtn.addEventListener('click',copyCodeCss)
+randomGeneratorBtn.addEventListener('click',generateCssCodeRandom)
 generateCssCode()
+
+console.log(Math.random());
+
