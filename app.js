538b11daa22fe1bcc9a8195f5aa52b256532ff01
maniraad <maniraad7@gmail.com>
2023-11-05 18:28:41 +0330

    add new btn

---
+++ app.js
@@ -0,0 +1,8 @@
+const $ = document;
+
+let colorInputA = $.querySelector('#color-a');
+let colorInputB = $.querySelector('#color-b');
+const buttons = $.querySelectorAll('.buttons')
+let body = $.body;
+
+console.log(colorInputA.value);
